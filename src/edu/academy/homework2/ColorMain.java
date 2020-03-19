ba992036d380959498d8645bea1c25a2d4155dbc
jmabracodabra <jmabracodabra@gmail.com>
2020-03-19 14:31:27 +0300

    Updated ColorMain.java

--- src/edu/academy/homework2/ColorMain.java
+++ src/edu/academy/homework2/ColorMain.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class ColorMain {
     public static void main(String[] args) {
-        System.out.println("Please, enter a number from 1 to 7");
+        System.out.println("Введите число от 1 до 7");
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
 
         Color color = new Color(num);
-        System.out.println("color name is " + color.getName() + ", color number is " + color.getNumber());
+        System.out.println("Название цвета " + color.getName() + ", номер цвета " + color.getNumber());
     }
 }
